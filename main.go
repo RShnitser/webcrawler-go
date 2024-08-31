1a360a1a77d665fab48ed6e36429585a366939db
RShnitser <romanshnitser@hotmail.com>
2024-08-31 15:09:40 -0400

    add commandline args

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main(){
-	fmt.Println("Hello, World!")
+	cmdArgs := os.Args[1:]
+	if len(cmdArgs) < 1{
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}else if len(cmdArgs) > 1{
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	fmt.Printf("starting crawl of %v\n", cmdArgs[0])
 }
