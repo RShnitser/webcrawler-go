69d8b03c61bc9df9d04d347070aee69c99c25bd8
RShnitser <romanshnitser@hotmail.com>
2024-08-31 11:03:01 -0400

    add get urls from html

---
+++ get_urls_from_html.go
@@ -0,0 +1,48 @@
+package main
+
+import(
+	"strings"
+	"fmt"
+	"golang.org/x/net/html"
+	"net/url"
+)
+
+func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error){
+	
+	parsedBase, err := url.Parse(rawBaseURL)
+	if err != nil{
+		return nil, fmt.Errorf("couldn't parse base URL: %w", err)
+	}
+	
+	
+	htmlReader := strings.NewReader(htmlBody)
+	htmlTree, err := html.Parse(htmlReader)
+	if err != nil{
+		return nil, fmt.Errorf("couldn't parse html body: %w", err)
+	}
+	
+	
+	var result []string
+	var traverseNodes func(*html.Node)
+	traverseNodes = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					parsedLink, err := url.Parse(a.Val)
+					if err != nil{
+						fmt.Printf("couldn't parse href '%v': %v\n", a.Val, err)
+						continue
+					}
+					resolvedURL := parsedBase.ResolveReference(parsedLink)
+					result = append(result, resolvedURL.String())
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			traverseNodes(c)
+		}
+	}
+	traverseNodes(htmlTree)
+
+	return result, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module crawler
 
 go 1.22.2
+
+require golang.org/x/net v0.28.0 // indirect
